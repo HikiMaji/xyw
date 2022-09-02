037e783c03ad36e5b8751fba2b4575cb57a74fa7
Yukino <Z1583218208@gmail.com>
2022-09-02 15:57:32 +0800

    修改网址匹配

--- xyw.js
+++ xyw.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        http://10.11.1.3/*
+// @match        http://*/a70.htm
 
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none

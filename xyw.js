e774b179a3ca091bf79aed8c13dc72bf4e21abaa
Yukino <Z1583218208@gmail.com>
2022-09-02 15:41:17 +0800

    编写xyw.js

--- xyw.js
+++ xyw.js
@@ -1,16 +1,24 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         xyw登录
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        http://*/*
+// @match        http://10.11.1.3/*
+
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
 (function() {
 
+    let username = "*****";
+    let password = "*****";
 
-    // Your code here...
+    let set = document.getElementsByClassName("edit_lobo_cell");
+
+
+    set[1].value = username;
+    set[2].value = password;
+
 })();
